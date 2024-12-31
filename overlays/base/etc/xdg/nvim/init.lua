a423526687218d6884ffad7e88c852c03d9904c3
James Harmison <jharmison@gmail.com>
2024-12-31 17:17:34 -0500

    rearrange a bit

--- overlays/base/etc/xdg/nvim/init.lua
+++ overlays/base/etc/xdg/nvim/init.lua
@@ -1,7 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*",
-  command = "Neotree show reveal_force_cwd",
-})
