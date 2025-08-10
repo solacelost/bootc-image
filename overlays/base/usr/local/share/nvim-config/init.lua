9b9229660ad717b04313a489ff3444491a1d12c4
James Harmison <jharmison@gmail.com>
2025-08-10 15:18:14 -0400

    add: split keybinds for mini.files

--- overlays/base/usr/local/share/nvim-config/init.lua
+++ overlays/base/usr/local/share/nvim-config/init.lua
@@ -119,6 +119,37 @@ later(function()
         },
     })
     map('n', '<leader>e', minifiles.open, { desc = 'explore' })
+    local map_split = function(buf_id, lhs, direction, close_on_file)
+      local rhs = function()
+        local new_target_window
+        local cur_target_window = minifiles.get_explorer_state().target_window
+        if cur_target_window ~= nil then
+          vim.api.nvim_win_call(cur_target_window, function()
+            vim.cmd("belowright " .. direction .. " split")
+            new_target_window = vim.api.nvim_get_current_win()
+          end)
+
+          minifiles.set_target_window(new_target_window)
+          minifiles.go_in({ close_on_file = close_on_file })
+        end
+      end
+
+      local desc = "Open in " .. direction .. " split"
+      if close_on_file then
+        desc = desc .. " and close"
+      end
+      vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        map_split(buf_id, "<C-w>s", "horizontal", false)
+        map_split(buf_id, "<C-w>v", "vertical", false)
+        map_split(buf_id, "<C-w>S", "horizontal", true)
+        map_split(buf_id, "<C-w>V", "vertical", true)
+      end,
+    })
 end)
 later(function()
     require 'treewalker'.setup()
