0fe768ad0efb2837f5a333311fed323f7b47da85
James Harmison <jharmison@gmail.com>
2025-10-25 20:49:00 -0400

    simplify vm script

--- hack/create_vm.sh
+++ hack/create_vm.sh
@@ -14,25 +14,12 @@ set -ex
 
 sudo cp -uf boot-image/bootc-install"${iso_suffix}".iso /var/lib/libvirt/images/
 
-second_disk="$name-1.qcow2"
-second_disk_source="$name-1.bak.qcow2"
-
-# If our source exists, wipe out the old disk and reclone it
-if virsh vol-list default | grep -qF "$second_disk_source"; then
-	virsh vol-delete "$second_disk" --pool default || :
-	virsh vol-clone "$second_disk_source" "$second_disk" --pool default
-# If our source doesn't exist, and the volume destination doesn't exist, create it as empty
-elif ! virsh vol-list default | grep -qF "$second_disk"; then
-	virsh vol-create-as --pool default --name "$second_disk" --capacity 10G
-fi
-
 virt-install --connect qemu:///system \
 	--virt-type=kvm --cpu=host-passthrough \
-	--video model=virtio --channel spicevmc \
+	--graphics spice \
 	--name "${name}" --memory "${memory_size}" \
-	--vcpus "${vcpus}" --osinfo fedora40 --sound default \
+	--vcpus "${vcpus}" --osinfo fedora-rawhide --sound default \
 	--disk "size=${disk_size},bus=virtio,cache=writethrough,io=threads" \
-	--disk "bus=virtio,cache=writethrough,io=threads,vol=default/${second_disk}" \
 	--controller type=scsi,model=virtio-scsi \
 	--channel unix,target_type=virtio,name=org.qemu.guest_agent.0 \
 	--cdrom /var/lib/libvirt/images/bootc-install"${iso_suffix}".iso \
